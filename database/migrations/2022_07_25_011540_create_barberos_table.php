082d10cc0dda4053185d311a11700814a5a9582e
miguel-jesus-js <winalllpz@gmail.com>
2022-10-03 01:32:51 -0500

    - correccion al registrar barberos y consultar citas
    Signed-off-by: miguel-jesus-js <winalllpz@gmail.com>

--- database/migrations/2022_07_25_011540_create_barberos_table.php
+++ database/migrations/2022_07_25_011540_create_barberos_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('foto');
-            $table->boolean('esAdmin');
+            $table->boolean('esAdmin')->default(false);
             $table->string('api_token')->nullable(true);
             $table->rememberToken();
             $table->timestamps();
